c695fdd7400b712381d0a6ae1930df453dd13c1b
Eloka Agu <eloka@satellitelabs.xyz>
2024-05-03 17:31:01 +0100

    vercel deploy

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,6 +3,7 @@ import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 import {muxInput} from 'sanity-plugin-mux-input'
+import {vercelDeployTool} from 'sanity-plugin-vercel-deploy'
 
 export default defineConfig({
   name: 'default',
@@ -11,7 +12,7 @@ export default defineConfig({
   projectId: '8rwgyjc1',
   dataset: 'production',
 
-  plugins: [structureTool(), visionTool(), muxInput()],
+  plugins: [structureTool(), visionTool(), muxInput(), vercelDeployTool()],
 
   schema: {
     types: schemaTypes,
